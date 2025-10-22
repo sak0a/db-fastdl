9c0f8e6ca2cd8a457a30f583292ff37a18f07198
sak0a <laurinfrank03@gmail.com>
2025-10-22 21:24:17 +0200

    chore: remove package-lock.json from db-fastdl to prevent unnecessary tracking of dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: false },
-    app: { baseURL: '/db-fastdl/' },
 
     routeRules: {
         // Disable homepage pre-rendered at build time
