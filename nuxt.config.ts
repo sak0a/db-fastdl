1b7c65057992de4390188555cdac048eca15c695
sak0a <laurinfrank03@gmail.com>
2025-10-22 21:48:51 +0200

    chore: update Nuxt baseURL and reorganize assets into the public directory

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: false },
-    app: { baseURL: '/db-fastdl' },
+    app: { baseURL: '/db-fastdl/' },
 
     routeRules: {
         // Disable homepage pre-rendered at build time
